be44842d0ba46cef95c8f27b4b3962bde3b20459
James Totah <jtotah@usfca.edu>
2020-11-10 18:30:03 -0800

    UPDATE: Added tasks that need to be completed

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,76 @@
+
+
+
+/**
+ * FIX: This event listener is broken, be sure it has access to the correct data
+ */
+$("#search").on("submit", function (e) {
+    e.preventDefault();
+    console.log(e.target.value);
+    webpageGenerator();
+})
+
+/**
+ * Fetch and fill country card
+ */
+let bannerFetch = function () {
+    fetch("URL")
+        .then(response => response.json())
+        .then(data => {
+            //FILL CODE
+        })
+}
+
+/**
+ * Fetch weather data and return it
+ */
+let weatherFetch = function () {
+    fetch("URL")
+        .then(response => response.json())
+        .then(data => {
+            return data;
+        })
+}
+
+/**
+ * Write a loop that fills appropriate number of cards with relevent weather data
+ */
+let weatherCards = function (weatherData) {
+    //Clear existing cards before adding more
+
+
+    for (let i = 0; i < weatherData.list.length; i++) {
+        //create and fill card elems here or write another function to call here accessing the weatherData parameter
+    }
+}
+
+let webpageGenerator = function () {
+    bannerFetch();
+    let weatherData = weatherFetch();
+    weatherCards(weatherData);
+}
+
+
+
+
+
+
+
+
+
+
+
+
 // fetch("https://api.openweathermap.org/data/2.5/forecast?q=Dallas&appid=b8cf73639b0d81c1905ba1ac1cb6f289")
 //     .then(response => response.json())
 //     .then(data => console.log(data));
+
+// fetch("https://restcountries.eu/rest/v2/name/usa")
+//     .then(response => response.json())
+//     .then(data => {
+//         $("#country-name").text(data[0].name);
+//         $("#country-name").attr("style", "text-align: center;");
+//         $("#nat-flag").attr("src", data[0].flag);
+//         $("#nat-flag").attr("style", "width: 300px;");
+//         $("#country-data").attr("style", "width: fit-content;");
+//     });
